eb26581c4238da249d2b62869b6917a81ccbda5e
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-12-04 19:40:40 +0600

    List

--- practice.dart
+++ practice.dart
@@ -8,8 +8,21 @@ void main() {
   var numbe = ['GP', 'Tele', 'Bangla', 'Robi'];
   print(numbe);
 
-  List<int> Listnumber = [1, 2, 3, 4, 5,5,5];
+  List<int> Listnumber = [1, 2, 3, 4, 5, 5, 5];
   Set<int> setNumber = {1, 2, 3, 4, 5, 3};
+  Set<int> setNumber1 = {1, 2, 3, 4, 5, 3};
   print(Listnumber);
-  print(setNumber);
+  print(setNumber.union(setNumber1));
+
+  var num = {
+    'name': "Sumon",
+    'age': 20,
+    'sex': 'male',
+  };
+  print(num['sex']);
+  num['location'] = 'Dhaka';
+  print(num);
+  var entity = num.keys.toList();
+  var attribute = num.values.toList();
+  print(attribute);
 }
