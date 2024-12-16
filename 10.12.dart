99d33e4bc133fbb0d5d771c5d92bb9a481b11058
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-12-17 00:43:40 +0600

    Encapsulation

--- 10.12.dart
+++ 10.12.dart
@@ -4,4 +4,17 @@ main() {
   print(age ?? 20);
   List<int?> nulllist = [10, 20, 30, 40, null];
   print(nulllist);
+  Student st = new Student();
+  print(st.getName('Sumon'));
+}
+
+class Student {
+  String getName(String name) {
+    _idandName(name);
+    return name;
+  }
+
+  _idandName(String name) {
+    print('$name and Id');
+  }
 }
